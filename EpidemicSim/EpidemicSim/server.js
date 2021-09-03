5fd430921696a231fa98fede043af4bd50b0b06d
Thanathorn Nilkhao <thanathorn@outlook.com>
2021-09-03 15:14:33 +0700

    Map test

--- EpidemicSim/EpidemicSim/server.js
+++ EpidemicSim/EpidemicSim/server.js
@@ -1,11 +1,11 @@
-'use strict';
+// 'use strict';
 
-const express = require('express');
-const app = express();
-var port = process.env.PORT || 1337;
+// const express = require('express');
+// const app = express();
+// var port = process.env.PORT || 1337;
 
-app.use(express.static('public'));
-app.listen(port);
+// app.use(express.static('public'));
+// app.listen(port);
 
 
 
