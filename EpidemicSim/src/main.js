50b2807a2cb83b2f9c8d7ba48c6ca7fa7f34a169
Akira <akira.rds01@gmail.com>
2021-09-15 11:31:56 +0700

    Initialize

--- EpidemicSim/src/main.js
+++ EpidemicSim/src/main.js
@@ -1,115 +1,11 @@
-// var vertexShader =
-//     [
-//         "precision mediump float;",
-//         "",
-//         "attribute vec2 vertPos;",
-//         "",
-//         "void main()",
-//         "{",
-//         "   gl_Position = vec4(vertPos, 0.0, 1.0);",
-//         "}",
-//     ].join("\n");
-
-// var fragmentShader =
-//     [
-//         "precision mediump float;",
-//         "",
-//         "void main()",
-//         "{",
-//         "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);",
-//         "}",
-//     ].join("\n");
-
-// var InitDemo = function ()
-// {
-//     console.log("This is working");
-
-//     var canvas = document.getElementById("WebGL-surface");
-//     var gl = canvas.getContext("webgl");
-
-//     if (!gl)
-//     {
-//         console.log("WebGL is not support without experimental version");
-//         gl = canvas.getContext("experimental-webgl");
-//     }
-//     if (!gl)
-//     {
-//         alert("Your Brower does not support WebGL");
-//     }
-
-//     gl.clearColor(0.1, 0, 0.1, 0.5);
-//     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-//     //Creating Shader
-//     var vertShader = gl.createShader(gl.VERTEX_SHADER);
-//     var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-//     gl.shaderSource(vertShader, vertexShader);
-//     gl.shaderSource(fragShader, fragmentShader);
-
-//     gl.compileShader(vertShader);
-//     if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS))
-//     {
-//         console.error("Error compiling vertex shader", gl.getShaderInfoLog(vertShader));
-//         return;
-//     }
-
-//     gl.compileShader(fragShader);
-//     if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS))
-//     {
-//         console.error("Error compiling fragment shader", gl.getShaderInfoLog(fragShader));
-//         return;
-//     }
-
-//     var program = gl.createProgram();
-//     gl.attachShader(program, vertShader);
-//     gl.attachShader(program, fragShader);
-//     gl.linkProgram(program);
-//     if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-//     {
-//         console.error("Error linking program", gl.getProgramInfoLog(program));
-//         return;
-//     }
-
-//     gl.validateProgram(program);
-//     if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS))
-//     {
-//         console.error("Error validating program", gl.getProgramInfoLog(program));
-//         return;
-//     }
-
-//     //Create Buffer
-//     var triangleVertices =
-//         [
-//             //x       y
-//             0.0, 0.5,
-//             -0.5, -0.5,
-//             0.5, -0.5
-//         ];
-
-//     var VBO = gl.createBuffer();
-//     gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
-//     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-
-//     var attributeLocation = gl.getAttribLocation(program, "vertPos");
-//     gl.vertexAttribPointer(
-//         attributeLocation,                  //Location
-//         2,                                  //Number of Element per Attribute           
-//         gl.FLOAT,                           //Type of Element
-//         gl.FALSE,
-//         2 * Float32Array.BYTES_PER_ELEMENT,  //Size of individual vertext
-//         0                                   //Offset from the beginning of data
-//     );
-
-//     gl.enableVertexAttribArray(attributeLocation);
-
-//     ///Main Render Loop
-//     gl.useProgram(program);
-//     gl.drawArrays(gl.TRIANGLES, 0, 3);
-// }
 
 let mapMarker;
 let placeService;
+
+const units = 100;
+const dests = 10;
+const maxdest = 3;
+const maxTime = 5;
 
 let disablePlaces = true;
 
@@ -141,7 +37,7 @@ var placeRequest =
 
 function InitMap()
 {
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+    //map = new google.maps.Map(document.getElementById('map'), mapOptions);
 
     if(disablePlaces === false)
     {
@@ -149,7 +45,42 @@ function InitMap()
         placeService.nearbySearch(placeRequest, SearchNearbyCallback);
     }
 
-    map.setCenter(latLng[0]);
+    //map.setCenter(latLng[0]);
+
+    console.time("init_time")
+
+    let manager = new Manager();
+
+    for(let i = 0; i < dests; i++){
+        manager.m_destList.push(Dest);
+    }
+
+    for(let i = 0; i < units; i++){
+        manager.m_unitList.push(new Unit(Math.floor(Math.random()* maxTime) + 1, Math.floor(Math.random()* maxTime) + 1));
+        manager.m_unitList[i].InitDest(manager.m_destList.length, maxdest);
+    }
+
+    console.timeEnd("init_time")
+
+
+    for(let i = 0; i < manager.m_unitList.length; i++){
+        document.write(manager.m_unitList[i].m_dest[0] + " " + manager.m_unitList[i].m_dest[1] + " " + manager.m_unitList[i].m_dest[2]);
+        document.write("<br>");
+        document.write("stay: " + manager.m_unitList[i].m_stayDelay + " trav:  " + manager.m_unitList[i].m_travDelay);
+        document.write("<br>");
+        document.write("<br>");
+    }
+
+    while(true){
+        
+        for(let i = 0; i < manager.m_unitList.length; i++){
+            manager.m_unitList[i].Update();
+        }
+
+    }
+
+    
+    
 }
 
 function CreateMarker(place)
--- EpidemicSim/src/objects.js
+++ EpidemicSim/src/objects.js
@@ -1,19 +1,40 @@
+// control the simulation
 
-// For better understanding purpose
-let unitList = [];  // for storing all unit
-let destList = [];  // for storing all dest
-
+function Manager() {
+    this.m_unitList = [],
+    this.m_destList = []
+}
 
 // test class 1
 // sus and inf in single object
 
-var Unit = {
-    m_state: 0,     // determine if infected or not ( if true => infected )
-    m_dest: [],     // array of destination that unit will take, store position of dest in destList[]
+// var Unit = {
+//     m_state: 0,     // determine if infected or not ( if true => infected )
+//     m_dest: [],     // array of destination that unit will take, store position of dest in destList[]
 
-    m_timeStay,     // time spent in one location   [fixed for now]
-    m_timeTrav     // time spent travel from one place to another   [fixed for now]
-}
+//     m_stayDelay: 0,     // time spent in one location   [fixed for now]
+//     m_travDelay: 0     // time spent travel from one place to another   [fixed for now]
+// }
+
+function Unit(arg_stay, arg_trav) {
+    this.m_state = 0,
+
+    this.m_dest = []
+    this.pos = 0;
+
+    this.m_stayDelay = arg_stay,
+    this.m_travDelay = arg_trav,
+
+    this.InitDest = function(dest_n, dest_max){
+        for(let i = 0; i < dest_max; i++){
+            this.m_dest.push(Math.floor(Math.random() * dest_n));
+        }
+    }
+
+    this.Update = function(){
+        
+    }
+  }
 
 // test class 2
 // separate sus and inf 
@@ -21,15 +42,15 @@ var Unit = {
 var Sus = {     // susceptible
     m_dest: [],     // array of destination that unit will take
 
-    m_timeStay,     // time spent in one location
-    m_timeTrav     // time spent travel from one place to another
+    m_stayDelay: 0,     // time spent in one location
+    m_travDelay: 0     // time spent travel from one place to another
 }
 
 var Inf = {     // infected
     m_dest: [],     // array of destination that unit will take
 
-    m_timeStay,     // time spent in one location
-    m_timeTrav      // time spent travel from one place to another
+    m_stayDelay: 0,     // time spent in one location
+    m_travDelay: 0      // time spent travel from one place to another
 }
 
 
