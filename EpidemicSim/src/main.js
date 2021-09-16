bc64bcce7062b4d9dc9bfa8b09de33910cd54a93
Thanathorn Nilkhao <thanathorn@outlook.com>
2021-09-16 23:32:46 +0700

    Implement Chart.js

--- EpidemicSim/src/main.js
+++ EpidemicSim/src/main.js
@@ -1,194 +1,57 @@
-// var vertexShader =
-//     [
-//         "precision mediump float;",
-//         "",
-//         "attribute vec2 vertPos;",
-//         "",
-//         "void main()",
-//         "{",
-//         "   gl_Position = vec4(vertPos, 0.0, 1.0);",
-//         "}",
-//     ].join("\n");
+var ctx = document.getElementById('myChart').getContext('2d');
 
-// var fragmentShader =
-//     [
-//         "precision mediump float;",
-//         "",
-//         "void main()",
-//         "{",
-//         "   gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);",
-//         "}",
-//     ].join("\n");
+var arr = [ 12, 15, 5, 6, 9, 20]
+var delayed;
 
-// var InitDemo = function ()
-// {
-//     console.log("This is working");
-
-//     var canvas = document.getElementById("WebGL-surface");
-//     var gl = canvas.getContext("webgl");
-
-//     if (!gl)
-//     {
-//         console.log("WebGL is not support without experimental version");
-//         gl = canvas.getContext("experimental-webgl");
-//     }
-//     if (!gl)
-//     {
-//         alert("Your Brower does not support WebGL");
-//     }
-
-//     gl.clearColor(0.1, 0, 0.1, 0.5);
-//     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-//     //Creating Shader
-//     var vertShader = gl.createShader(gl.VERTEX_SHADER);
-//     var fragShader = gl.createShader(gl.FRAGMENT_SHADER);
-
-//     gl.shaderSource(vertShader, vertexShader);
-//     gl.shaderSource(fragShader, fragmentShader);
-
-//     gl.compileShader(vertShader);
-//     if (!gl.getShaderParameter(vertShader, gl.COMPILE_STATUS))
-//     {
-//         console.error("Error compiling vertex shader", gl.getShaderInfoLog(vertShader));
-//         return;
-//     }
-
-//     gl.compileShader(fragShader);
-//     if (!gl.getShaderParameter(fragShader, gl.COMPILE_STATUS))
-//     {
-//         console.error("Error compiling fragment shader", gl.getShaderInfoLog(fragShader));
-//         return;
-//     }
-
-//     var program = gl.createProgram();
-//     gl.attachShader(program, vertShader);
-//     gl.attachShader(program, fragShader);
-//     gl.linkProgram(program);
-//     if (!gl.getProgramParameter(program, gl.LINK_STATUS))
-//     {
-//         console.error("Error linking program", gl.getProgramInfoLog(program));
-//         return;
-//     }
-
-//     gl.validateProgram(program);
-//     if(!gl.getProgramParameter(program, gl.VALIDATE_STATUS))
-//     {
-//         console.error("Error validating program", gl.getProgramInfoLog(program));
-//         return;
-//     }
-
-//     //Create Buffer
-//     var triangleVertices =
-//         [
-//             //x       y
-//             0.0, 0.5,
-//             -0.5, -0.5,
-//             0.5, -0.5
-//         ];
-
-//     var VBO = gl.createBuffer();
-//     gl.bindBuffer(gl.ARRAY_BUFFER, VBO);
-//     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(triangleVertices), gl.STATIC_DRAW);
-
-//     var attributeLocation = gl.getAttribLocation(program, "vertPos");
-//     gl.vertexAttribPointer(
-//         attributeLocation,                  //Location
-//         2,                                  //Number of Element per Attribute           
-//         gl.FLOAT,                           //Type of Element
-//         gl.FALSE,
-//         2 * Float32Array.BYTES_PER_ELEMENT,  //Size of individual vertext
-//         0                                   //Offset from the beginning of data
-//     );
-
-//     gl.enableVertexAttribArray(attributeLocation);
-
-//     ///Main Render Loop
-//     gl.useProgram(program);
-//     gl.drawArrays(gl.TRIANGLES, 0, 3);
-// }
-
-let mapMarker;
-let placeService;
-
-let disablePlaces = true;
-
-const latLng = 
-[
-    { lat: 13.760908340064168, lng: 100.50323524044852 },
-    { lat: 13.796141420615395, lng: 100.5179707956749},
-    { lat: 13.773579874674216, lng: 100.51335956111564},
-    { lat: 13.765024187194893, lng: 100.53825408115135},
-];
-const image = "Ahegaopng.png";
-
-var mapOptions = 
-{
-    center: latLng[0],
-    // Zoom range: 0-22
-    zoom: 12,
-    mapId: "5afdd176907dbee8",
-    fullscreenControl: false,
-    mapTypeControl: false,
-    streetViewControl: false,
-};
-var placeRequest = 
-{
-    location: latLng[0],
-    radius: 1000,
-    type: ["resturant"],
-};
-
-function InitMap()
-{
-    map = new google.maps.Map(document.getElementById('map'), mapOptions);
-
-    if(disablePlaces === false)
+var myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: arr,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.8)',
+                'rgba(54, 162, 235, 0.8)',
+                'rgba(255, 206, 86, 0.8)',
+                'rgba(75, 192, 192, 0.8)',
+                'rgba(153, 102, 255, 0.8)', 
+                'rgba(255, 159, 64, 0.8)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: 
     {
-        placeService = new google.maps.places.PlacesService(map);
-        placeService.nearbySearch(placeRequest, SearchNearbyCallback);
-    }
-
-    map.setCenter(latLng[0]);
-}
-
-function CreateMarker(place)
-{
-    if(!place.geometry || !place.geometry.location) return;
-
-    mapMarker = new google.maps.Marker
-    ({
-        map,
-        draggable: false,
-        animation: google.maps.Animation.DROP,
-        position: place.geometry.location,
-        title: "AHEGAO",
-        icon: 
+        animation: 
         {
-            url: image,
-            scaledSize: new google.maps.Size(80, 80)
+            onComplete: () => {
+                delayed = true;
+            },
+            delay: (context) => {
+                let delay = 0;
+                if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                }
+                return delay;
+            },
         },
-    });
-
-    mapMarker.addListener("click", ToggleBounce);
-
-}
-
-function ToggleBounce()
-{
-    if(mapMarker.getAnimation() !== null)
-        mapMarker.setAnimation(null);
-    else
-        mapMarker.setAnimation(google.maps.Animation.BOUNCE);
-}
-
-function SearchNearbyCallback(results, status)
-{
-    if (status == google.maps.places.PlacesServiceStatus.OK) 
-    {
-        for (var i = 0; i < results.length; i++) 
+        scales: 
         {
-          CreateMarker(results[i]);
+            x: {
+              stacked: true,
+            },
+            y: {
+              stacked: true
+            }
         }
     }
-}
+});
