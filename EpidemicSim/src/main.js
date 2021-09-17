5d2db350adbb6a30e3c19a09d6379406396ec2db
Thanathorn Nilkhao <thanathorn@outlook.com>
2021-09-17 15:48:28 +0700

    Test the implementation of HTML Form and JQuery

--- EpidemicSim/src/main.js
+++ EpidemicSim/src/main.js
@@ -1,57 +1,68 @@
 var ctx = document.getElementById('myChart').getContext('2d');
 
-var arr = [ 12, 15, 5, 6, 9, 20]
-var delayed;
+var x;
 
-var myChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: arr,
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.8)',
-                'rgba(54, 162, 235, 0.8)',
-                'rgba(255, 206, 86, 0.8)',
-                'rgba(75, 192, 192, 0.8)',
-                'rgba(153, 102, 255, 0.8)', 
-                'rgba(255, 159, 64, 0.8)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
-    },
-    options: 
-    {
-        animation: 
-        {
-            onComplete: () => {
-                delayed = true;
-            },
-            delay: (context) => {
-                let delay = 0;
-                if (context.type === 'data' && context.mode === 'default' && !delayed) {
-                delay = context.dataIndex * 300 + context.datasetIndex * 100;
-                }
-                return delay;
-            },
+function SetInt()
+{
+    x = document.getElementById("xVal").value;
+    x = parseInt(x);
+}
+
+function Entry()
+{
+    var arr = [ 12, 15, 5, 6, 9, x];
+    var delayed;
+
+    var myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: arr,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.8)',
+                    'rgba(54, 162, 235, 0.8)',
+                    'rgba(255, 206, 86, 0.8)',
+                    'rgba(75, 192, 192, 0.8)',
+                    'rgba(153, 102, 255, 0.8)', 
+                    'rgba(255, 159, 64, 0.8)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
         },
-        scales: 
+        options: 
         {
-            x: {
-              stacked: true,
+            animation: 
+            {
+                onComplete: () => {
+                    delayed = true;
+                },
+                delay: (context) => {
+                    let delay = 0;
+                    if (context.type === 'data' && context.mode === 'default' && !delayed) {
+                    delay = context.dataIndex * 300 + context.datasetIndex * 100;
+                    }
+                    return delay;
+                },
             },
-            y: {
-              stacked: true
+            scales: 
+            {
+                x: {
+                  stacked: true,
+                },
+                y: {
+                  stacked: true
+                }
             }
         }
-    }
-});
+    });
+}
