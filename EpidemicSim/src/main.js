965a595f2da1b35031e9a2e973f630174c8c6112
Thanathorn Nilkhao <thanathorn@outlook.com>
2021-10-22 14:36:12 +0700

    TripsLayer implementation (no animations)

--- EpidemicSim/src/main.js
+++ EpidemicSim/src/main.js
@@ -7,6 +7,9 @@ const GOOGLE_MAPS_API_KEY = "AIzaSyDEQPWQuG15KfetsMZM2jPzrwJyz0vAdAc"; // eslint
 const GOOGLE_MAP_ID = "5afdd176907dbee8"; // eslint-disable-line
 const GOOGLE_MAPS_API_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDEQPWQuG15KfetsMZM2jPzrwJyz0vAdAc&map_ids=5afdd176907dbee8&v=beta";
 
+const DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf.trips.json";
+const LOOP_LENGTH = 1800;
+
 function loadScript(url) {
   const script = document.createElement('script');
   script.type = 'text/javascript';
@@ -21,15 +24,57 @@ function loadScript(url) {
 loadScript(GOOGLE_MAPS_API_URL).then(() => 
 {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 51.47, lng: 0.45},
-    zoom: 5,
+    center: { lat: 37.77605537998559, lng: -122.41466001900622 },
+    tilt: 45,
+    zoom: 15,
     mapId: GOOGLE_MAP_ID
   });
 
   // Create overlay instance
-  const overlay = new GoogleMapsOverlay
+  const overlay = new GoogleMapsOverlay({});
+
+  const tripsLayer = new TripsLayer
   ({
-      layers: []
+    id: "TripsLayer",
+    data: DATA_URL,
+     /* props from TripsLayer class */
+  
+    currentTime: 500,
+    // fadeTrail: true,
+    getTimestamps: d => d.waypoints.map(p => p.timestamp - 1554772579000),
+    trailLength: 600,
+    
+    /* props inherited from PathLayer class */
+    
+    // billboard: false,
+    capRounded: true,
+    getColor: [0, 255, 0],
+    getPath: d => d.waypoints.map(p => p.coordinates),
+    // getWidth: 1,
+    jointRounded: true,
+    // miterLimit: 4,
+    // rounded: true,
+    // widthMaxPixels: Number.MAX_SAFE_INTEGER,
+    widthMinPixels: 8,
+    // widthScale: 1,
+    // widthUnits: 'meters',
+    
+    /* props inherited from Layer class */
+    
+    // autoHighlight: false,
+    // coordinateOrigin: [0, 0, 0],
+    // coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
+    // highlightColor: [0, 0, 128, 128],
+    // modelMatrix: null,
+    opacity: 0.8,
+    // pickable: false,
+    // visible: true,
+    // wrapLongitude: false,
+  });
+
+  overlay.setProps
+  ({
+    layers: [tripsLayer],
   });
 
   // Add overlay to map
