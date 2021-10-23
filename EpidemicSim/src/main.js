b66f1ffb5d1176a49f4366f017b081045b556d26
Thanathorn Nilkhao <thanathorn@outlook.com>
2021-10-23 10:42:48 +0700

    Test animation with many agents

--- EpidemicSim/src/main.js
+++ EpidemicSim/src/main.js
@@ -7,8 +7,15 @@ const GOOGLE_MAPS_API_KEY = "AIzaSyDEQPWQuG15KfetsMZM2jPzrwJyz0vAdAc"; // eslint
 const GOOGLE_MAP_ID = "5afdd176907dbee8"; // eslint-disable-line
 const GOOGLE_MAPS_API_URL = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDEQPWQuG15KfetsMZM2jPzrwJyz0vAdAc&map_ids=5afdd176907dbee8&v=beta";
 
-const DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf.trips.json";
+//const DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/website/sf.trips.json";
+const DATA_URL = "https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/trips/trips-v7.json";
 const LOOP_LENGTH = 1800;
+
+const VENDOR_COLORS = 
+[
+  [0, 66, 176],
+  [190, 25, 25], // vendor #1
+];
 
 function loadScript(url) {
   const script = document.createElement('script');
@@ -24,7 +31,8 @@ function loadScript(url) {
 loadScript(GOOGLE_MAPS_API_URL).then(() => 
 {
   const map = new google.maps.Map(document.getElementById('map'), {
-    center: { lat: 37.77605537998559, lng: -122.41466001900622 },
+    //center: { lat: 37.77605537998559, lng: -122.41466001900622 },
+    center: { lat: 40.72, lng: -74 },
     tilt: 45,
     zoom: 15,
     mapId: GOOGLE_MAP_ID
@@ -33,44 +41,58 @@ loadScript(GOOGLE_MAPS_API_URL).then(() =>
   // Create overlay instance
   const overlay = new GoogleMapsOverlay({});
 
-  const props = 
-    {
-      id: "TripsLayer",
-      data: DATA_URL,
-      /* props from TripsLayer class */
-      fadeTrail: true,
-      getTimestamps: d => d.waypoints.map(p => p.timestamp - 1554772579000),
-      trailLength: 50,
-      
-      /* props inherited from PathLayer class */
-      
-      // billboard: false,
-      capRounded: true,
-      getColor: [255, 0, 255],
-      getPath: d => d.waypoints.map(p => p.coordinates),
-      // getWidth: 1,
-      jointRounded: true,
-      // miterLimit: 4,
-      rounded: true,
-      // widthMaxPixels: Number.MAX_SAFE_INTEGER,
-      widthMinPixels: 8,
-      // widthScale: 1,
-      // widthUnits: 'meters',
-      
-      /* props inherited from Layer class */
-      
-      // autoHighlight: false,
-      // coordinateOrigin: [0, 0, 0],
-      // coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
-      // highlightColor: [0, 0, 128, 128],
-      // modelMatrix: null,
-      opacity: 1,
-      // pickable: false,
-      // visible: true,
-      // wrapLongitude: false,
-    }
-
   let currentTime = 0;
+
+  // const props = 
+  // {
+  //   id: "TripsLayer",
+  //   data: DATA_URL,
+  //   /* props from TripsLayer class */
+  //   fadeTrail: true,
+  //   getTimestamps: d => d.waypoints.map(p => p.timestamp - 1554772579000),
+  //   trailLength: 50,
+    
+  //   /* props inherited from PathLayer class */
+    
+  //   // billboard: false,
+  //   capRounded: true,
+  //   getColor: [255, 0, 255],
+  //   getPath: d => d.waypoints.map(p => p.coordinates),
+  //   // getWidth: 1,
+  //   jointRounded: true,
+  //   // miterLimit: 4,
+  //   rounded: true,
+  //   // widthMaxPixels: Number.MAX_SAFE_INTEGER,
+  //   widthMinPixels: 8,
+  //   // widthScale: 1,
+  //   // widthUnits: 'meters',
+    
+  //   /* props inherited from Layer class */
+    
+  //   // autoHighlight: false,
+  //   // coordinateOrigin: [0, 0, 0],
+  //   // coordinateSystem: COORDINATE_SYSTEM.LNGLAT,
+  //   // highlightColor: [0, 0, 128, 128],
+  //   // modelMatrix: null,
+  //   opacity: 1,
+  //   // pickable: false,
+  //   // visible: true,
+  //   // wrapLongitude: false,
+  // }
+
+  const props = {
+    id: "trips",
+    data: DATA_URL,
+    getPath: (d) => d.path,
+    getTimestamps: (d) => d.timestamps,
+    getColor: (d) => VENDOR_COLORS[d.vendor],
+    opacity: 1,
+    widthMinPixels: 2,
+    trailLength: 20,
+    fadeTrail: true,
+    currentTime,
+    shadowEnabled: false,
+  };
 
   const animate = () => 
   {
