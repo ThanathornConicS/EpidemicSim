c50577f3d49756b50a970f1b3ffc062bab52c8c4
Thanathorn Nilkhao <thanathorn@outlook.com>
2022-05-20 15:17:51 +0700

    Finish Implementing Chart

--- EpidemicSim/src/ChartModule.js
+++ EpidemicSim/src/ChartModule.js
@@ -1,6 +1,6 @@
 //import { Chart } from "chart.js";
 
-
+const lebels = ['1', '2', '3', '4', '5', '6'];
 const ctx = document.getElementById('chartCanvas').getContext('2d');
 const plugin = {
     id: 'custom_canvas_background_color',
@@ -14,35 +14,56 @@ const plugin = {
     }
 };
 let config = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: lebels,
+        datasets: 
+        [
+            {
+                order: 2,
+                label: 'Susceptible',
+                data: [20, 19, 15, 10, 4, 0],
+                //borderColor: "rgb(0, 0, 255)",
+                backgroundColor: "rgb(51,145,215)",
+                hoverBorderWidth: 1,
+                hoverBackgroundColor: "rgb(0, 0, 0)",
+                hoverBorderColor: "rgb(0, 0, 255)",
+                pointRadius: 3,
+                fill: true,
+                //borderWidth: 0.7,
+            },
+            {
+                order: 1,
+                label: 'Infected',
+                data: [0, 1, 3, 8, 10, 11],
+                //borderColor: "rgb(255, 0, 0)",
+                backgroundColor: "rgb(242,102,91)",
+                hoverBorderWidth: 1,
+                hoverBackgroundColor: "rgb(0, 0, 0)",
+                hoverBorderColor: "rgb(255, 0, 0)",
+                pointRadius: 3,
+                fill: true,
+                //borderWidth: 0.7,
+            },
+            {
+                order: 0,
+                label: 'Removed',
+                data: [0, 0, 2, 2, 6, 9],
+                //borderColor: "rgb(50, 50, 50)",
+                backgroundColor: "rgb(153,153,153)",
+                hoverBorderWidth: 1,
+                hoverBackgroundColor: "rgb(0, 0, 0)",
+                hoverBorderColor: "rgb(50, 50, 50)",
+                pointRadius: 3,
+                fill: true,
+                //borderWidth: 0.7,
+            }
+        ]
     },
     options: 
     {
         responsive: true,
-        maintainAspectRatio: false,
+        maintainAspectRatio: true,
         layout: 
         {
             padding:
